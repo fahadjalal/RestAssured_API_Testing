27af4da2105042ec9463adfa8496946bb4a15b82
JalalFahad <fahadjalal02@gmail.com>
2023-08-25 20:36:30 +0500

    PUt, POST, DELETE added

--- src/test/java/Assignment_1/Task_1.java
+++ src/test/java/Assignment_1/Task_1.java
@@ -1,11 +1,43 @@
 package Assignment_1;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.hasItem;
 
 public class Task_1 {
     @Test
     public void Get_Items(){
+        RestAssured.baseURI = "https://reqres.in/api";
+       Response response = RestAssured.given()
+               .get("/users?page=2")
+               .then()
+               .extract()
+               .response();
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200, "Status code is not 200");
+        System.out.println(response.statusCode());
 
+        int expectedId = 9;
+        String expectedFirstName = "Tobias";
+
+        int actualId = response.path("data[2].id");
+        String actualFirstName = response.path("data[2].first_name");
+
+        System.out.println(actualId);
+        System.out.println(actualFirstName);
+
+        Assert.assertEquals(actualId, expectedId, "ID doesn't match");
+        Assert.assertEquals(actualFirstName, expectedFirstName, "First name doesn't match");
+
+
+        response.then()
+                .body("data.email", hasItem("michael.lawson@reqres.in"))
+                .body("data.last_name", hasItem("Fields"))
+                .body("data.last_name", hasItem("Howell"))
+                .log().all();
 
     }
 }
--- src/test/java/Assignment_1/Task_2.java
+++ src/test/java/Assignment_1/Task_2.java
@@ -1,8 +1,55 @@
 package Assignment_1;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class Task_2 {
-    public void Delete_Item()
-    {
+    @Test
+    public void Delete_Item() {
+
+        //DELETE Call and Verify Status Code
+        RestAssured.baseURI = "https://reqres.in/api";
+        Response deleteResponse = given()
+                .when()
+                .delete("/users/2")
+                .then()
+                .extract()
+                .response();
+
+        int deleteStatusCode = deleteResponse.getStatusCode();
+        System.out.println("DELETE Status Code: " + deleteStatusCode);
 
     }
+
+    //POST Call and Verification
+    @Test
+    public void Post_Item() {
+        String requestWithJson = "{ \"name\": \"fahJa\", \"year\": \"2020\" }";
+        RestAssured.baseURI = "https://reqres.in/api";
+        Response postResponse = given()
+                .body(requestWithJson.toString())
+                .when()
+                .post("/register")
+                .then()
+                .extract()
+                .response();
+        postResponse.then().log().all();
+
+
+       String token = postResponse.jsonPath().getString("token");
+       System.out.println("Token Value: " + token);
+
+       Assert.assertNotNull(token, "Token value is null");
+       Assert.assertFalse(token.isEmpty(), "Token value is empty");
+
+       long responseTime = postResponse.time();
+       Assert.assertTrue(responseTime < 10000, "Response time is greater than 10 seconds");
+    }
+
 }
+
